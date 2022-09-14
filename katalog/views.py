39c4a6844ebfd7d21123468595cfa7e135788360
Valee <valeriansalim@gmail.com>
2022-09-14 16:17:52 +0700

    Added context

--- katalog/views.py
+++ katalog/views.py
@@ -6,6 +6,15 @@ from .models import CatalogItem
 class IndexView(generic.ListView):
     template_name = 'katalog.html'
     context_object_name = 'katalog_list'
-
+    
     def get_queryset(self):
         return CatalogItem.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'katalog_list': CatalogItem.objects.all(),
+            'nama': 'Valerian Salim',
+            'npm': '2106630012'
+        })
+        return context        
